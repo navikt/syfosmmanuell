3040ab2d226a0cf28430fb30cb24196998754010
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-09-12 16:10:07 +0200

    endre til mer leselige css klassenavn

--- src/components/SMTilbakedatert.tsx
+++ src/components/SMTilbakedatert.tsx
@@ -4,15 +4,15 @@ import { Element, Undertittel, Normaltekst } from 'nav-frontend-typografi';
 import './Sykmelding.less';
 
 const SMTilbakedatert: React.FC<{ sykmelding: Sykmelding}> = ({sykmelding}) => (
-    <ul className="sm-konteiner">
-        <li className="sm-konteiner__sm-tittel sm-konteiner--style-type-none">
+    <ul className="sykmelding">
+        <li className="sykmelding__tittel liste-element">
             <Undertittel>Sykmelding</Undertittel>
         </li>
-        <li className="sm-konteiner__sm-element sm-konteiner--style-type-none">
+        <li className="sykmelding__element liste-element">
             <Element>ID</Element>
             <Normaltekst>{sykmelding.id}</Normaltekst>
         </li>
-        <li className="sm-konteiner__sm-element sm-konteiner--style-type-none">
+        <li className="sykmelding__element liste-element">
             <Element>Message ID</Element>
             <Normaltekst>{sykmelding.msgId}</Normaltekst>
         </li>
