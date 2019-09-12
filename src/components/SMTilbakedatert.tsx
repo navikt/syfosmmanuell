3495a4e700a6f1295b3e8187d9c5b24c854cb1cb
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-09-12 13:34:37 +0200

    implisitt return i funksjon som kun returnerer tsx

--- src/components/SMTilbakedatert.tsx
+++ src/components/SMTilbakedatert.tsx
@@ -3,25 +3,24 @@ import { Sykmelding_t } from '../types/sykmeldingTypes';
 import { Element, Undertittel, Normaltekst } from 'nav-frontend-typografi';
 import './Sykmelding.less';
 
-const SMTilbakedatert: React.FC<{ sykmelding: Sykmelding_t}> = ({sykmelding}) => {
-    return (
-        <ul className="sm-container">
-            {/* Husk å legg til tags på alle tekstfelt */}
-            <li className="sm-container-item">
-                <Undertittel>Sykmelding</Undertittel>
-            </li>
-            <li className="sm-container-item">
-                <Element>ID</Element>
-                <Normaltekst>{sykmelding.id}</Normaltekst>
-            </li>
-            <li className="sm-container-item">
-                <Element>Message ID</Element>
-                <Normaltekst>{sykmelding.msgId}</Normaltekst>
-            </li>
-            {/* Add all listitems with correct formatting */}
-        </ul>
-    )
-}
+const SMTilbakedatert: React.FC<{ sykmelding: Sykmelding_t}> = ({sykmelding}) => (
+    <ul className="sm-container">
+        {/* Husk å legg til tags på alle tekstfelt */}
+        <li className="sm-container-item">
+            <Undertittel>Sykmelding</Undertittel>
+        </li>
+        <li className="sm-container-item">
+            <Element>ID</Element>
+            <Normaltekst>{sykmelding.id}</Normaltekst>
+        </li>
+        <li className="sm-container-item">
+            <Element>Message ID</Element>
+            <Normaltekst>{sykmelding.msgId}</Normaltekst>
+        </li>
+        {/* Add all listitems with correct formatting */}
+    </ul>
+)
+
 
 
 export default SMTilbakedatert;
