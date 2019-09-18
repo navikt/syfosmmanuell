8e47a68da3e207d3370bfd72f99793380f853aa0
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-09-18 15:35:17 +0200

    refakturerer SykmeldingWrapper

--- src/components/SykmeldingWrapper.tsx
+++ src/components/SykmeldingWrapper.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { useEffect } from 'react';
-import { RuleNames } from '../types/ValidationResultTypes';
 import useFetchSykmelding from '../hooks/useFetchSykmelding';
 import NavFrontendSpinner from 'nav-frontend-spinner';
 import SykmeldingVisning from './SykmeldingVisning';
@@ -46,51 +45,7 @@ const SykmeldingWrapper = () => {
                 </div>  
             }
         </>
-    )
-
-
-
-    if (isLoading) { 
-        return (
-            <div className="spinner">
-                <NavFrontendSpinner/>
-            </div>
-        )
-    }
-    else if (arsaker) {
-        console.log(sykmelding)
-        switch (arsaker.ruleHits[0].ruleName) {
-            case RuleNames.TILBAKEDATERT_MED_BEGRUNNELSE_FORSTE_SYKMELDING: {
-                return (
-                    <div className="ekspanderbartpanel-konteiner">
-                        <EkspanderbartpanelBase heading={
-                            <div className="ekspanderbartpanel">
-                                <img src="src/img/report-problem-circle.svg" alt="Alert image" className="ekspanderbartpanel__ikon"/>
-                                <Undertittel>En sykmelding må vurderes manuelt</Undertittel>
-                            </div>
-                        }>
-                            <SykmeldingVisning sykmelding={sykmelding}/>
-                        <Knapper begrunnelse={"RuleNames.TILBAKEDATERT_MED_BEGRUNNELSE_FORSTE_SYKMELDING"} handterAvgjorelse={handterAvgjorelse} handterAvbryt={handterAvbryt}/>
-                        </EkspanderbartpanelBase>
-                    </div>
-                )
-            }
-            case RuleNames.TILBAKEDATERT_MED_BEGRUNNELSE_FORLENGELSE: {
-                return <></>
-            }
-            case RuleNames.BEHANDLER_KI_FT_MT_BENYTTER_ANNEN_DIAGNOSEKODE_ENN_L: {
-                return <></>
-            } 
-            case RuleNames.AVVENTENDE_SYKMELDING_KOMBINERT: {
-                return <></>
-            }
-            default:
-                return (<><p>hello</p></>)
-        }
-    }
-    else {
-        return <p>nothing to show</p>
-    } 
+    ) 
 }
 
 export default SykmeldingWrapper;
