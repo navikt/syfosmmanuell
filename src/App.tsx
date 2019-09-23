f057e3ef3f29015e80702553f87963e1cc176caa
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-09-23 12:37:24 +0200

    flytter innhold fra Sykmeldingwrapper til App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
 import * as React from 'react';
-import SykmeldingWrapper from './components/SykmeldingWrapper'
+import { useEffect } from 'react';
+import useFetchSykmelding from './hooks/useFetchSykmelding';
+import NavFrontendSpinner from 'nav-frontend-spinner';
+import { EkspanderbartpanelBase } from 'nav-frontend-ekspanderbartpanel';
+import { Undertittel } from 'nav-frontend-typografi';
+import { ValidationResult } from './types/ValidationResultTypes';
+import ArsakBehandling from './components/ArsakBehandling';
 import './App.less';
 
 const App = () => {
-    return(
+    const { arsaker, sykmelding, error, isLoading, callFetch } = useFetchSykmelding();
+
+    const handterAvgjorelse = ( arsaker: ValidationResult, erGodkjent: boolean) => {
+        // set arsakErVurdert til true/false
+
+        console.log("Avgjørelse håndteres i wrapper: " + erGodkjent);
+    }
+
+    const handterAvbryt = () => {
+        console.log("Avbryt håndteres i wrapper ")
+    }
+
+    useEffect( () => {
+        callFetch('src/mock/sykmeld.json');
+    }, []);
+    
+    return (
         <>
-            <SykmeldingWrapper/>
+            {isLoading && <div className="spinner"><NavFrontendSpinner/></div>}
+            {!isLoading && arsaker &&
+                <div className="ekspanderbartpanel">
+                <EkspanderbartpanelBase heading={
+                    <div className="ekspanderbartpanel__header">
+                        <img src="src/img/report-problem-circle.svg" alt="Alert image" className="ekspanderbartpanel__ikon"/>
+                        <Undertittel>En sykmelding må vurderes manuelt</Undertittel>
+                    </div>
+                }>
+                    <ArsakBehandling arsaker={arsaker} sykmelding={sykmelding} handterFerdigstill={handterAvgjorelse} handterAvbryt={handterAvbryt} />
+                </EkspanderbartpanelBase>
+                </div>
+            }
         </>
-    )
+    ) 
 }
 
 export default App;
