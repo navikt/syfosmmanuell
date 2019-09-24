56ac09d5711d0fb8dd31632626ed7813b61583d5
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-09-24 09:22:26 +0200

    rydder opp i asakbehandling

--- src/components/ArsakBehandling.tsx
+++ src/components/ArsakBehandling.tsx
@@ -65,102 +65,96 @@ const ArsakBehandling: React.FC<ArsakBehandlingProps> = ({
         setCurrentArsak(null);
     };
 
-    if (!arsaker) {
-        return <></>;
-    } else if (arsaker.ruleHits.length == 1) {
-        return (
-            <div className="sykmelding-visning">
-                <SykmeldingVisning arsaker={arsaker} sykmelding={sykmelding} />
-                <Knapper
-                    regel={arsaker.ruleHits[0].ruleName}
-                    knappeTekst={KnappeTekst.FERDIGSTILL}
-                    handterAvgjorelse={oppdaterArsakVurdering}
-                    handterAvbryt={arsakVurderingAvbrutt}
-                />
-            </div>
-        );
-    } else if (arsaker.ruleHits.length > 1) {
-        return (
-            <>
-                {!currentArsak && arsakVurdering && (
-                    <>
-                        <div className="arsak-visning">
-                            <Element>Årsaker til manuell behandling</Element>
-                            <ol className="liste arsak-liste">
-                                {arsaker.ruleHits.map((arsak, index) => {
-                                    return (
-                                        <li key={index} className="liste__element arsak-liste__element">
-                                            <div className="arsak-liste__element--number">
-                                                {<Element>{index + 1}.</Element>}
-                                            </div>
-                                            <div className="arsak-liste__element--venstre">
-                                                {<Normaltekst>{arsak.ruleName}.</Normaltekst>}
-                                            </div>
-                                            <div className="arsak-liste__element--hoyre">
-                                                {arsakVurdering.get(arsak.ruleName) == true && (
-                                                    <img
-                                                        src="src/img/check-circle-2.svg"
-                                                        alt="Declined icon"
-                                                        className="ikon"
-                                                    />
-                                                )}
-                                                {arsakVurdering.get(arsak.ruleName) == false && (
-                                                    <img
-                                                        src="src/img/cross-circle.svg"
-                                                        alt="Declined icon"
-                                                        className="ikon"
-                                                    />
-                                                )}
-                                                {arsakVurdering.get(arsak.ruleName) == null && (
-                                                    <Knapp
-                                                        form="kompakt"
-                                                        htmlType="button"
-                                                        onClick={(): void => setCurrentArsak(arsak.ruleName)}
-                                                    >
-                                                        Vurder
-                                                    </Knapp>
-                                                )}
-                                            </div>
-                                        </li>
-                                    );
-                                })}
-                            </ol>
-                        </div>
-                        <div className="innsending">
-                            {totalVurdering == null && (
-                                <Knapp disabled htmlType="button" className="innsending__ferdigstill">
-                                    {KnappeTekst.FERDIGSTILL}
-                                </Knapp>
-                            )}
-                            {totalVurdering != null && (
-                                <Knapp
-                                    htmlType="button"
-                                    onClick={(): void => handterFerdigstill(arsaker, totalVurdering)}
-                                >
-                                    {KnappeTekst.FERDIGSTILL}
-                                </Knapp>
-                            )}
-                            <Flatknapp onClick={handterAvbryt} className="innsending__avbryt">
-                                Avbryt
-                            </Flatknapp>
-                        </div>
-                    </>
-                )}
-                {currentArsak && arsakVurdering && (
-                    <div className="sykmelding-visning">
-                        <SykmeldingVisning arsaker={arsaker} sykmelding={sykmelding} />
-                        <Knapper
-                            regel={currentArsak}
-                            knappeTekst={KnappeTekst.LAGRE}
-                            handterAvgjorelse={oppdaterArsakVurdering}
-                            handterAvbryt={arsakVurderingAvbrutt}
-                        />
+    const enkelArsakVisning = (
+        <div className="sykmelding-visning">
+            <SykmeldingVisning arsaker={arsaker} sykmelding={sykmelding} />
+            <Knapper
+                regel={arsaker.ruleHits[0].ruleName}
+                knappeTekst={KnappeTekst.FERDIGSTILL}
+                handterAvgjorelse={oppdaterArsakVurdering}
+                handterAvbryt={arsakVurderingAvbrutt}
+            />
+        </div>
+    );
+
+    const flerarsakVisning = (
+        <>
+            {!currentArsak && arsakVurdering && (
+                <>
+                    <div className="arsak-visning">
+                        <Element>Årsaker til manuell behandling</Element>
+                        <ol className="liste arsak-liste">
+                            {arsaker.ruleHits.map((arsak, index) => {
+                                return (
+                                    <li key={index} className="liste__element arsak-liste__element">
+                                        <div className="arsak-liste__element--number">
+                                            {<Element>{index + 1}.</Element>}
+                                        </div>
+                                        <div className="arsak-liste__element--venstre">
+                                            {<Normaltekst>{arsak.ruleName}.</Normaltekst>}
+                                        </div>
+                                        <div className="arsak-liste__element--hoyre">
+                                            {arsakVurdering.get(arsak.ruleName) == true && (
+                                                <img
+                                                    src="src/img/check-circle-2.svg"
+                                                    alt="Declined icon"
+                                                    className="ikon"
+                                                />
+                                            )}
+                                            {arsakVurdering.get(arsak.ruleName) == false && (
+                                                <img
+                                                    src="src/img/cross-circle.svg"
+                                                    alt="Declined icon"
+                                                    className="ikon"
+                                                />
+                                            )}
+                                            {arsakVurdering.get(arsak.ruleName) == null && (
+                                                <Knapp
+                                                    form="kompakt"
+                                                    htmlType="button"
+                                                    onClick={(): void => setCurrentArsak(arsak.ruleName)}
+                                                >
+                                                    Vurder
+                                                </Knapp>
+                                            )}
+                                        </div>
+                                    </li>
+                                );
+                            })}
+                        </ol>
                     </div>
-                )}
-            </>
-        );
-    }
-    return <></>;
+                    <div className="innsending">
+                        {totalVurdering == null && (
+                            <Knapp disabled htmlType="button" className="innsending__ferdigstill">
+                                {KnappeTekst.FERDIGSTILL}
+                            </Knapp>
+                        )}
+                        {totalVurdering != null && (
+                            <Knapp htmlType="button" onClick={(): void => handterFerdigstill(arsaker, totalVurdering)}>
+                                {KnappeTekst.FERDIGSTILL}
+                            </Knapp>
+                        )}
+                        <Flatknapp onClick={handterAvbryt} className="innsending__avbryt">
+                            Avbryt
+                        </Flatknapp>
+                    </div>
+                </>
+            )}
+            {currentArsak && arsakVurdering && (
+                <div className="sykmelding-visning">
+                    <SykmeldingVisning arsaker={arsaker} sykmelding={sykmelding} />
+                    <Knapper
+                        regel={currentArsak}
+                        knappeTekst={KnappeTekst.LAGRE}
+                        handterAvgjorelse={oppdaterArsakVurdering}
+                        handterAvbryt={arsakVurderingAvbrutt}
+                    />
+                </div>
+            )}
+        </>
+    );
+
+    return <>{arsaker.ruleHits.length == 1 ? enkelArsakVisning : flerarsakVisning}</>;
 };
 
 export default ArsakBehandling;
