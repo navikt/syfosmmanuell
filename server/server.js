7065b21d4a6d2b4114d202e8f939330dfa30818a
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-10-04 12:18:06 +0200

    endrer hostname

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const express = require('express');
 //const dist = path.join(__dirname, 'dist');
 
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = '127.0.0.1';
 
 const app = express();
 
-app.use('/person', express.static(__dirname + '/dist'));
+app.use('/person', express.static('dist'));
 
 app.get('/is_alive', (req, res) => {
     res.sendStatus(200);
