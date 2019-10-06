8b405389db8815eddfcbab020cf5c2392d36a6d1
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-10-06 18:12:04 +0200

    kiropraktor sykemelding builder

--- src/components/SykmeldingVisning.tsx
+++ src/components/SykmeldingVisning.tsx
@@ -10,11 +10,56 @@ const SykmeldingVisning: React.FC = () => {
         aktuellManOppgave: { sykmelding },
     } = useAppStore();
 
+    const kiropraktorBuilder = (): JSX.Element => (
+        <>
+            <div className="grid-item">
+                <Element>Grad</Element>
+                <Normaltekst>{sykmelding.perioder[0].gradert.grad}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Når startet det legemeldte sykefraværet?</Element>
+                <Normaltekst>{dayjs(sykmelding.syketilfelleStartDato).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item--left">
+                <Element>Sykmeldingsperiode</Element>
+                <Normaltekst>
+                    {dayjs(sykmelding.perioder[0].fom).format('DD.MM.YYYY') +
+                        ' - ' +
+                        dayjs(sykmelding.perioder[0].tom).format('DD.MM.YYYY')}
+                </Normaltekst>
+            </div>
+            <div className="grid-item grid-item--right">
+                <Element>Sykmeldingsdato</Element>
+                <Normaltekst>{dayjs(sykmelding.signaturDato).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item--left">
+                <Element>Hoveddiagnose</Element>
+                <Normaltekst>{'hvor finner man dette?'}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item--right">
+                <Element>Kode</Element>
+                <Normaltekst>{sykmelding.medisinskVurdering.hovedDiagnose.kode}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item--left">
+                <Element>Bidiagnose</Element>
+                {sykmelding.medisinskVurdering.biDiagnoser.map((diagnose, index) => (
+                    <Normaltekst key={index}>{'hvor finner man dette?'}</Normaltekst>
+                ))}
+            </div>
+            <div className="grid-item grid-item--right">
+                <Element>Kode</Element>
+                {sykmelding.medisinskVurdering.biDiagnoser.map((diagnose, index) => (
+                    <Normaltekst key={index}>{diagnose.kode}</Normaltekst>
+                ))}
+            </div>
+        </>
+    );
+
     const flerePerioderBuilder = (): JSX.Element => (
         <>
-            <div className="grid-item grid-item__tittel">
+            <div className="grid-item">
                 <Element>Når startet det legemeldte sykefraværet?</Element>
-                <Normaltekst>{sykmelding.id}</Normaltekst>
+                <Normaltekst>{dayjs(sykmelding.syketilfelleStartDato).format('DD.MM.YYYY')}</Normaltekst>
             </div>
             <div className="grid-item grid-item--left">
                 <Element>Sykmeldingsperiode</Element>
@@ -55,7 +100,7 @@ const SykmeldingVisning: React.FC = () => {
                 <div className="grid-item grid-item__tittel">
                     <Systemtittel>Sykmelding</Systemtittel>
                 </div>
-                {flerePerioderBuilder()}
+                {kiropraktorBuilder()}
             </div>
         </>
     );
