dba42f02c9c164a6a7ce35b7ad6f6932b8edc4ec
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-10-06 19:37:18 +0200

    alle sykmelding builders ferdig

--- src/components/SykmeldingVisning.tsx
+++ src/components/SykmeldingVisning.tsx
@@ -10,6 +10,215 @@ const SykmeldingVisning: React.FC = () => {
         aktuellManOppgave: { sykmelding },
     } = useAppStore();
 
+    const tilbakedatertLopendePeriodeBuilder = (): JSX.Element => (
+        <>
+            <div className="grid-item">
+                <Element>Grad</Element>
+                <Normaltekst>{sykmelding.perioder[0].gradert.grad + ' %'}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Når startet det legemeldte sykefraværet?</Element>
+                <Normaltekst>{dayjs(sykmelding.syketilfelleStartDato).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item--left">
+                <Element>Sykmeldingsperiode</Element>
+                <Normaltekst>
+                    {dayjs(sykmelding.perioder[0].fom).format('DD.MM.YYYY') +
+                        ' - ' +
+                        dayjs(sykmelding.perioder[0].tom).format('DD.MM.YYYY')}
+                </Normaltekst>
+            </div>
+            <div className="grid-item grid-item--right">
+                <Element>Sykmeldingsdato</Element>
+                <Normaltekst>{dayjs(sykmelding.signaturDato).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Dato for dokumenterbar kontakt med pasienten</Element>
+                <Normaltekst>{dayjs(sykmelding.kontaktMedPasient.kontaktDato).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Pasienten har ikke kunne ivareta egne interesser. Begrunn</Element>
+                <Normaltekst>{sykmelding.kontaktMedPasient.begrunnelseIkkeKontakt}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item--left">
+                <Element>Hoveddiagnose</Element>
+                <Normaltekst>{'hvor finner man dette?'}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item--right">
+                <Element>Kode</Element>
+                <Normaltekst>{sykmelding.medisinskVurdering.hovedDiagnose.kode}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item-tittel">
+                <Undertittel>Friskmelding/prognose</Undertittel>
+            </div>
+            <div className="grid-item checkbox">
+                <img src="src/img/check-box.svg" alt="Checkbox icon" className="checkbox__ikon" />
+                <div className="checkbox__tekst">
+                    <Normaltekst>Pasienten er 100% arbeidsfør etter denne perioden</Normaltekst>
+                </div>
+            </div>
+            <div className="grid-item">
+                <Element>Beskriv eventuelle hensyn som må tas på arbeidsplassen</Element>
+                <Normaltekst>{sykmelding.prognose.hensynArbeidsplassen}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Pasient med arbeidsgiver: ...</Element>
+                <div className="checkbox">
+                    <img src="src/img/check-box.svg" alt="Checkbox icon" className="checkbox__ikon" />
+                    <div className="checkbox__tekst">
+                        <Normaltekst>
+                            Jeg antar at pasienten på sikt kan komme tilbake til samme arbeidsgiver
+                        </Normaltekst>
+                    </div>
+                </div>
+            </div>
+            <div className="grid-item">
+                <Element>Anslå når du tror dette kan skje</Element>
+                <Normaltekst>{dayjs(sykmelding.prognose.erIArbeid.arbeidFOM).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            <div className="grid-item checkbox">
+                <img src="src/img/check-box.svg" alt="Checkbox icon" className="checkbox__ikon" />
+                <div className="checkbox__tekst">
+                    <Normaltekst>Jeg antar at pasienten på sikt kan komme i arbeid hos annen arbeidsgiver</Normaltekst>
+                </div>
+            </div>
+            <div className="grid-item">
+                <Element>Hvis usikker: Når antar du å kunne gi tilbakemelding på dette?</Element>
+                <Normaltekst>{dayjs(sykmelding.prognose.erIArbeid.vurderingsdato).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            {/* SPØR OM DENNE SEKSJONEN */}
+            <div className="grid-item grid-item-tittel">
+                <Undertittel>Utdypende opplysninger</Undertittel>
+                <Element>Helseopplysninger ved vurdering av aktivitetskravet</Element>
+            </div>
+            <div className="grid-item">
+                <Element>Beskriv kort sykehistorie, symptomer og funn i dagens situasjon</Element>
+                <Normaltekst>{sykmelding.tiltakNAV}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>
+                    Beskriv pågående og planlagt henvisning, utredning og/eller behandling. Lar dette seg kombinere med
+                    delvis arbeid?
+                </Element>
+                <Normaltekst>{sykmelding.andreTiltak}</Normaltekst>
+            </div>
+            {/* SPØR OM DENNE SEKSJONEN */}
+            <div className="grid-item grid-item-tittel">
+                <Undertittel>Hva skal til for å bedre arbeidsevnen?</Undertittel>
+            </div>
+            <div className="grid-item">
+                <Element>
+                    Tilrettelegging/hensyn som bør tas på arbeidsplassen. Beskriv (kan leses av arbeidsgiver)
+                </Element>
+                <Normaltekst>{sykmelding.tiltakArbeidsplassen}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Tiltak i regi av NAV. Beskriv. (Hvis der er behov fra NAV nå, bruk felt 8.)</Element>
+                <Normaltekst>{sykmelding.tiltakNAV}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Eventuelle andre innspill til NAV. Beskriv</Element>
+                <Normaltekst>{sykmelding.andreTiltak}</Normaltekst>
+            </div>
+        </>
+    );
+
+    const tilbakedatertBuilder = (): JSX.Element => (
+        <>
+            <div className="grid-item">
+                <Element>Grad</Element>
+                <Normaltekst>{sykmelding.perioder[0].gradert.grad + ' %'}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Når startet det legemeldte sykefraværet?</Element>
+                <Normaltekst>{dayjs(sykmelding.syketilfelleStartDato).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item--left">
+                <Element>Sykmeldingsperiode</Element>
+                <Normaltekst>
+                    {dayjs(sykmelding.perioder[0].fom).format('DD.MM.YYYY') +
+                        ' - ' +
+                        dayjs(sykmelding.perioder[0].tom).format('DD.MM.YYYY')}
+                </Normaltekst>
+            </div>
+            <div className="grid-item grid-item--right">
+                <Element>Sykmeldingsdato</Element>
+                <Normaltekst>{dayjs(sykmelding.signaturDato).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Dato for dokumenterbar kontakt med pasienten</Element>
+                <Normaltekst>{dayjs(sykmelding.kontaktMedPasient.kontaktDato).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Pasienten har ikke kunne ivareta egne interesser. Begrunn</Element>
+                <Normaltekst>{sykmelding.kontaktMedPasient.begrunnelseIkkeKontakt}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item--left">
+                <Element>Hoveddiagnose</Element>
+                <Normaltekst>{'hvor finner man dette?'}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item--right">
+                <Element>Kode</Element>
+                <Normaltekst>{sykmelding.medisinskVurdering.hovedDiagnose.kode}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item-tittel">
+                <Undertittel>Friskmelding/prognose</Undertittel>
+            </div>
+            <div className="grid-item checkbox">
+                <img src="src/img/check-box.svg" alt="Checkbox icon" className="checkbox__ikon" />
+                <div className="checkbox__tekst">
+                    <Normaltekst>Pasienten er 100% arbeidsfør etter denne perioden</Normaltekst>
+                </div>
+            </div>
+            <div className="grid-item">
+                <Element>Beskriv eventuelle hensyn som må tas på arbeidsplassen</Element>
+                <Normaltekst>{sykmelding.prognose.hensynArbeidsplassen}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Pasient med arbeidsgiver: ...</Element>
+                <div className="checkbox">
+                    <img src="src/img/check-box.svg" alt="Checkbox icon" className="checkbox__ikon" />
+                    <div className="checkbox__tekst">
+                        <Normaltekst>
+                            Jeg antar at pasienten på sikt kan komme tilbake til samme arbeidsgiver
+                        </Normaltekst>
+                    </div>
+                </div>
+            </div>
+            <div className="grid-item">
+                <Element>Anslå når du tror dette kan skje</Element>
+                <Normaltekst>{dayjs(sykmelding.prognose.erIArbeid.arbeidFOM).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            <div className="grid-item checkbox">
+                <img src="src/img/check-box.svg" alt="Checkbox icon" className="checkbox__ikon" />
+                <div className="checkbox__tekst">
+                    <Normaltekst>Jeg antar at pasienten på sikt kan komme i arbeid hos annen arbeidsgiver</Normaltekst>
+                </div>
+            </div>
+            <div className="grid-item">
+                <Element>Hvis usikker: Når antar du å kunne gi tilbakemelding på dette?</Element>
+                <Normaltekst>{dayjs(sykmelding.prognose.erIArbeid.vurderingsdato).format('DD.MM.YYYY')}</Normaltekst>
+            </div>
+            <div className="grid-item grid-item-tittel">
+                <Undertittel>Hva skal til for å bedre arbeidsevnen?</Undertittel>
+            </div>
+            <div className="grid-item">
+                <Element>
+                    Tilrettelegging/hensyn som bør tas på arbeidsplassen. Beskriv (kan leses av arbeidsgiver)
+                </Element>
+                <Normaltekst>{sykmelding.tiltakArbeidsplassen}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Tiltak i regi av NAV. Beskriv. (Hvis der er behov fra NAV nå, bruk felt 8.)</Element>
+                <Normaltekst>{sykmelding.tiltakNAV}</Normaltekst>
+            </div>
+            <div className="grid-item">
+                <Element>Eventuelle andre innspill til NAV. Beskriv</Element>
+                <Normaltekst>{sykmelding.andreTiltak}</Normaltekst>
+            </div>
+        </>
+    );
+
     const kiropraktorBuilder = (): JSX.Element => (
         <>
             <div className="grid-item">
@@ -100,7 +309,7 @@ const SykmeldingVisning: React.FC = () => {
                 <div className="grid-item grid-item__tittel">
                     <Systemtittel>Sykmelding</Systemtittel>
                 </div>
-                {kiropraktorBuilder()}
+                {tilbakedatertLopendePeriodeBuilder()}
             </div>
         </>
     );
