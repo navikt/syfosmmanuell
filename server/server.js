48b7583bc84cee486cdcf04a953ec7843b0085a5
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-10-07 10:26:43 +0200

    legger til cors config

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ const port = 8080;
 app.get('/is_alive', (req, res) => res.sendStatus(200));
 app.get('/is_ready', (req, res) => res.sendStatus(200));
 
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+};
+app.use(cors(corsOptions));
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
