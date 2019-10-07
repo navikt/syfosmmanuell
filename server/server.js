7a474b4caf13a2441536441a2c0ae190edf61405
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-10-07 09:33:54 +0200

    legger til cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+
 const app = express();
 const port = 8080;
 
 app.get('/is_alive', (req, res) => res.sendStatus(200));
 app.get('/is_ready', (req, res) => res.sendStatus(200));
 
+app.use(cors());
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
