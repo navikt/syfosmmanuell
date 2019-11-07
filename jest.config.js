7917e018099816ddb81f7d4db8ce09f67c97f3b8
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-11-07 12:05:23 +0100

    fikser mock for svg filer når man tester med jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    "roots": [
-      "./src/tests"
-    ],
-    "transform": {
-      "^.+\\.tsx?$": "ts-jest"
+    roots: ['./src/tests'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.(css|less)$': './styleMock.js',
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': './fileMock.js',
     },
-    "setupFilesAfterEnv": ['./setupJest.ts']
-  }
+
+    setupFilesAfterEnv: ['./setupJest.ts'],
+};
