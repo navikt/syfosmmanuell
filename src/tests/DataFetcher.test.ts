216faa71e8aef0c3cf537080a73a02d3314b8df0
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-12-11 14:30:39 +0100

    bytter fra pnr til fnr

--- src/tests/DataFetcher.test.ts
+++ src/tests/DataFetcher.test.ts
@@ -2,35 +2,35 @@ import { hentUrlParameter } from '../utils/urlParameter';
 
 describe('DataFetcher', () => {
     describe('hentUrlParameter', () => {
-        test('Skal returnere object med prop "pnr" for url="url.com/?pnr=123"', () => {
-            const url = 'url.com/?pnr=123';
-            expect(hentUrlParameter(url)).toHaveProperty('pnr');
+        test('Skal returnere object med prop "fnr" for url="url.com/?fnr=123"', () => {
+            const url = 'url.com/?fnr=123';
+            expect(hentUrlParameter(url)).toHaveProperty('fnr');
         });
-        test('Skal returnere string for url="url.com/?pnr=123"', () => {
-            const url = 'url.com/?pnr=123';
-            expect(typeof hentUrlParameter(url).pnr).toBe('string');
+        test('Skal returnere string for url="url.com/?fnr=123"', () => {
+            const url = 'url.com/?fnr=123';
+            expect(typeof hentUrlParameter(url).fnr).toBe('string');
         });
         test('throws Error ved for url uten parametre', () => {
             const url = 'url.com/';
             expect(() => hentUrlParameter(url)).toThrowError(new Error('Url does not contain any parameters'));
         });
         test('throws Error ved for url med flere "?"', () => {
-            const url = 'url.com/?pnr=123?something';
+            const url = 'url.com/?fnr=123?something';
             expect(() => hentUrlParameter(url)).toThrowError(new Error('Url contains several "?"'));
         });
         test('throws Error ved for url med for mange parametre', () => {
-            const url = 'url.com/?pnr=123&param=value';
+            const url = 'url.com/?fnr=123&param=value';
             expect(() => hentUrlParameter(url)).toThrowError(new Error('Url contains too many parameters'));
         });
         test('throws Error ved for url for parameter uten verdi', () => {
-            const url = 'url.com/?pnr=';
-            const url2 = 'url.com/?pnr';
+            const url = 'url.com/?fnr=';
+            const url2 = 'url.com/?fnr';
             expect(() => hentUrlParameter(url)).toThrowError(new Error('Parameter does not contain any value'));
             expect(() => hentUrlParameter(url2)).toThrowError(new Error('Parameter does not contain any value'));
         });
-        test('throws Error ved for url hvor parameter ikke matcher "pnr"', () => {
+        test('throws Error ved for url hvor parameter ikke matcher "fnr"', () => {
             const url = 'url.com/?param=value';
-            expect(() => hentUrlParameter(url)).toThrowError(new Error('The parameter given is not pnr'));
+            expect(() => hentUrlParameter(url)).toThrowError(new Error('The parameter given is not fnr'));
         });
     });
 });
