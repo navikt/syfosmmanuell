2093be7fcd7229356160375f578a4a3e1bf0ad5e
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-01-08 13:05:58 +0100

    legger til håndtering for array respons fra backend

--- src/App.tsx
+++ src/App.tsx
@@ -10,103 +10,103 @@ import { ValidationResult, Status } from './types/validationresultTypes';
 import { Undertittel } from 'nav-frontend-typografi';
 
 const App = () => {
-    const [manOppgave, setManOppgave] = useState<ManuellOppgave | null>();
-    const [feilMelding, setFeilmelding] = useState<string | null>(null);
-    const manOppgaveFetcher = useFetch<ManuellOppgave>();
-    const manOppgavePutter = useFetch<ManuellOppgave>();
+  const [manOppgave, setManOppgave] = useState<ManuellOppgave | null>();
+  const [feilMelding, setFeilmelding] = useState<string | null>(null);
+  const manOppgaveFetcher = useFetch<ManuellOppgave[]>();
+  const manOppgavePutter = useFetch<ManuellOppgave>();
 
-    useEffect(() => {
-        if (isNotStarted(manOppgaveFetcher)) {
-            let OPPGAVE_ID = '';
-            try {
-                OPPGAVE_ID = hentOppgaveidFraUrlParameter(window.location.href);
-            } catch (error) {
-                console.error(error);
-            }
-            const URL = hentOppgaveUrl(OPPGAVE_ID);
-            console.log(URL);
-            manOppgaveFetcher.fetch(URL, undefined, (fetchState: FetchState<ManuellOppgave>) => {
-                if (!fetchState.data) {
-                    setFeilmelding('Ingen oppgaver funnet');
-                    console.error('Ingen oppgave funnet');
-                }
-                try {
-                    setManOppgave(new ManuellOppgave(fetchState.data));
-                } catch (error) {
-                    setFeilmelding('Kunne ikke formattere manuell oppgave.');
-                    console.error(error);
-                }
-            });
+  useEffect(() => {
+    if (isNotStarted(manOppgaveFetcher)) {
+      let OPPGAVE_ID = '';
+      try {
+        OPPGAVE_ID = hentOppgaveidFraUrlParameter(window.location.href);
+      } catch (error) {
+        console.error(error);
+      }
+      const URL = hentOppgaveUrl(OPPGAVE_ID);
+      console.log(URL);
+      manOppgaveFetcher.fetch(URL, undefined, (fetchState: FetchState<ManuellOppgave[]>) => {
+        if (!fetchState.data) {
+          setFeilmelding('Ingen oppgaver funnet');
+          console.error('Ingen oppgave funnet');
         }
-    }, [manOppgaveFetcher]);
-
-    const handterAvgjorelse = (avgjorelse: boolean | undefined): void => {
-        if (avgjorelse === undefined) {
-            // Skal ikke kunne skje
-            console.error('Avgjørelse ble satt til "undefined"');
+        try {
+          setManOppgave(new ManuellOppgave(fetchState.data?.pop));
+        } catch (error) {
+          setFeilmelding('Kunne ikke formattere manuell oppgave.');
+          console.error(error);
         }
-        if (manOppgave) {
-            if (isNotStartedOrPending(manOppgavePutter)) {
-                const URL = hentOppgaveUrlPut(manOppgave.oppgaveid);
-                const resultat = new ValidationResult({
-                    status: avgjorelse ? Status.OK : Status.INVALID,
-                    ruleHits: manOppgave.validationResult.ruleHits,
-                });
-                manOppgavePutter.fetch(
-                    URL,
-                    {
-                        method: 'PUT',
-                        headers: { 'Content-Type': 'application/json' },
-                        body: JSON.stringify(resultat),
-                    },
-                    () => {
-                        setManOppgave(null);
-                    },
-                );
-            }
-        } else {
-            console.error('Manuell oppgave ble ikke funnet');
-        }
-    };
-
-    if (feilMelding) {
-        return <p>{feilMelding}</p>;
+      });
     }
+  }, [manOppgaveFetcher]);
 
-    if (manOppgave === null) {
-        return <Undertittel>Oppgaven er løst</Undertittel>;
+  const handterAvgjorelse = (avgjorelse: boolean | undefined): void => {
+    if (avgjorelse === undefined) {
+      // Skal ikke kunne skje
+      console.error('Avgjørelse ble satt til "undefined"');
     }
-
-    if (isAnyPending([manOppgaveFetcher, manOppgavePutter])) {
-        return (
-            <div style={{ marginLeft: '2rem', marginTop: '2rem' }}>
-                <Spinner />
-            </div>
-        );
-    }
-
-    if (manOppgave?.validationResult.ruleHits.length === 1) {
-        return (
-            <EnRegel
-                sykmelding={manOppgave.sykmelding}
-                regel={manOppgave.validationResult.ruleHits[0].ruleName}
-                handterAvgjorelse={handterAvgjorelse}
-                handterAvbryt={() =>
-                    setFeilmelding(
-                        'Du har avbrutt oppgaven. Du kan enten lukke vinduet, eller laste inn siden på nytt for hente oppgaven tilbake.',
-                    )
-                }
-            />
-        );
-    }
-
     if (manOppgave) {
-        if (manOppgave.validationResult.ruleHits.length > 1) {
-            return <FlereRegler manOppgave={manOppgave} handterAvgjorelse={handterAvgjorelse} />;
-        }
+      if (isNotStartedOrPending(manOppgavePutter)) {
+        const URL = hentOppgaveUrlPut(manOppgave.oppgaveid);
+        const resultat = new ValidationResult({
+          status: avgjorelse ? Status.OK : Status.INVALID,
+          ruleHits: manOppgave.validationResult.ruleHits,
+        });
+        manOppgavePutter.fetch(
+          URL,
+          {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(resultat),
+          },
+          () => {
+            setManOppgave(null);
+          },
+        );
+      }
+    } else {
+      console.error('Manuell oppgave ble ikke funnet');
     }
+  };
 
-    return <p>Ukjent feil</p>;
+  if (feilMelding) {
+    return <p>{feilMelding}</p>;
+  }
+
+  if (manOppgave === null) {
+    return <Undertittel>Oppgaven er løst</Undertittel>;
+  }
+
+  if (isAnyPending([manOppgaveFetcher, manOppgavePutter])) {
+    return (
+      <div style={{ marginLeft: '2rem', marginTop: '2rem' }}>
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (manOppgave?.validationResult.ruleHits.length === 1) {
+    return (
+      <EnRegel
+        sykmelding={manOppgave.sykmelding}
+        regel={manOppgave.validationResult.ruleHits[0].ruleName}
+        handterAvgjorelse={handterAvgjorelse}
+        handterAvbryt={() =>
+          setFeilmelding(
+            'Du har avbrutt oppgaven. Du kan enten lukke vinduet, eller laste inn siden på nytt for hente oppgaven tilbake.',
+          )
+        }
+      />
+    );
+  }
+
+  if (manOppgave) {
+    if (manOppgave.validationResult.ruleHits.length > 1) {
+      return <FlereRegler manOppgave={manOppgave} handterAvgjorelse={handterAvgjorelse} />;
+    }
+  }
+
+  return <p>Ukjent feil</p>;
 };
 
 export default App;
