d5a047c9c28289eb839eb018ea6c068b09903cc1
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-01-08 13:36:50 +0100

    logging

--- src/types/manuellOppgaveTypes.ts
+++ src/types/manuellOppgaveTypes.ts
@@ -2,15 +2,16 @@ import { ValidationResultWithStatus } from './validationresultTypes';
 import { Sykmelding } from './sykmeldingTypes';
 
 export class ManuellOppgave {
-    oppgaveid: number;
-    validationResult: ValidationResultWithStatus;
-    sykmelding: Sykmelding;
+  oppgaveid: number;
+  validationResult: ValidationResultWithStatus;
+  sykmelding: Sykmelding;
 
-    constructor(manuellOppgave: any) {
-        this.oppgaveid = manuellOppgave.oppgaveid;
-        this.validationResult = new ValidationResultWithStatus(manuellOppgave.validationResult);
-        this.sykmelding = manuellOppgave.receivedSykmelding
-            ? new Sykmelding(manuellOppgave.receivedSykmelding.sykmelding)
-            : new Sykmelding(manuellOppgave.sykmelding);
-    }
+  constructor(manuellOppgave: any) {
+    console.log(manuellOppgave);
+    this.oppgaveid = manuellOppgave.oppgaveid;
+    this.validationResult = new ValidationResultWithStatus(manuellOppgave.validationResult);
+    this.sykmelding = manuellOppgave.receivedSykmelding
+      ? new Sykmelding(manuellOppgave.receivedSykmelding.sykmelding)
+      : new Sykmelding(manuellOppgave.sykmelding);
+  }
 }
