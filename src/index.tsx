7cfceb8e4d61fa1cce6e92cd9cf084483f0a1ecd
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-01-09 09:14:12 +0100

    env variable test

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.less';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+console.log('NODE_ENV: ' + process.env.NODE_ENV);
+console.log('REACT_APP_NODE_ENV: ' + process.env.REACT_APP_NODE_ENV);
+
 if (process.env.NODE_ENV === 'development') {
     require('./mock');
 }
