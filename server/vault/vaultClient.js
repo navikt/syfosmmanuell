3c2464bbe02af23b427fa76932a692ccd774afd2
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-01-13 14:58:15 +0100

    mer logging

--- server/vault/vaultClient.js
+++ server/vault/vaultClient.js
@@ -4,7 +4,10 @@ const setupVault = () => {
   const CLIENT_ID = fs.readFileSync('/secrets/azuread/syfosmmanuell/client_id');
   const CLIENT_SECRET = fs.readFileSync('/secrets/azuread/syfosmmanuell/client_secret');
   const BACKEND_CLIENT_ID = fs.readFileSync('/secrets/azuread/syfosmmanuell-backend/client_id');
-
+  console.log(CLIENT_ID);
+  console.log(CLIENT_SECRET);
+  console.log(BACKEND_CLIENT_ID);
+  
   try {
     process.env['CLIENT_ID'] = CLIENT_ID;
     process.env['CLIENT_SECRET'] = CLIENT_SECRET;
