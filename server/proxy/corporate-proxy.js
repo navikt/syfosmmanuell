5fb88334e332757fdd520b9198f0325d31db3464
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-01-14 14:59:16 +0100

    certificate

--- server/proxy/corporate-proxy.js
+++ server/proxy/corporate-proxy.js
@@ -3,34 +3,35 @@ import { custom } from 'openid-client';
 import tunnel from 'tunnel';
 
 const agent = proxyUri => {
-    if (proxyUri) {
-        const hostPort = proxyUri
-            .replace('https://', '')
-            .replace('http://', '')
-            .split(":", 2);
-        return tunnel.httpsOverHttp({
-            proxy: {
-                host: hostPort[0],
-                port: hostPort[1]
-            }
-        })
-    } else {
-        return null
-    }
+  if (proxyUri) {
+    const hostPort = proxyUri
+      .replace('https://', '')
+      .replace('http://', '')
+      .split(':', 2);
+    return tunnel.httpsOverHttp({
+      proxy: {
+        host: hostPort[0],
+        port: hostPort[1],
+      },
+      rejectUnauthorized: false,
+    });
+  } else {
+    return null;
+  }
 };
 
-const setup = (object) => {
-    const proxyUri = config.server.proxy;
-    const proxyAgent = agent(proxyUri);
-    if (proxyAgent) {
-        console.log(`Proxying requests via ${proxyUri} for '${object.constructor.name}'`);
-        object[custom.http_options] = options => {
-            options.agent = proxyAgent;
-            return options;
-        };
-    } else {
-        console.log(`Environment variable HTTP_PROXY is not set, not proxying requests for '${object.constructor.name}'`);
-    }
+const setup = object => {
+  const proxyUri = config.server.proxy;
+  const proxyAgent = agent(proxyUri);
+  if (proxyAgent) {
+    console.log(`Proxying requests via ${proxyUri} for '${object.constructor.name}'`);
+    object[custom.http_options] = options => {
+      options.agent = proxyAgent;
+      return options;
+    };
+  } else {
+    console.log(`Environment variable HTTP_PROXY is not set, not proxying requests for '${object.constructor.name}'`);
+  }
 };
 
-export default { setup }
+export default { setup };
