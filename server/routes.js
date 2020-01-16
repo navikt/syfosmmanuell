b7ce28ca88c3717b976e2a63ccdce24eaa42b677
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-01-16 16:09:40 +0100

    logger req.user

--- server/routes.js
+++ server/routes.js
@@ -45,6 +45,7 @@ const setup = authClient => {
   router.use('/', express.static(path.join(__dirname, 'build')));
 
   router.get('/user', (req, res) => {
+    console.log(req.user);
     if (!req.user && !req.user.tokenSet && !req.user.tokenSet.access_token) {
       res.status(500).send();
     }
