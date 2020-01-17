39dbbc4b804e7b39983bf373fbedc790ca3ada66
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-01-17 14:32:49 +0100

    Endrer constructor for calidationResult

--- src/components/sykmelding/SykmeldingHeader.tsx
+++ src/components/sykmelding/SykmeldingHeader.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { RuleNames } from '../../types/validationresultTypes';
+import { RuleNames, RuleNamesDescription } from '../../types/validationresultTypes';
 import { Element, Normaltekst } from 'nav-frontend-typografi';
 
 interface SykmeldingheaderProps {
-    regel: RuleNames;
-    arbeidsgiver?: string;
-    sykmelder: string;
+  regel: RuleNames;
+  arbeidsgiver?: string;
+  sykmelder: string;
 }
 
 const Sykmeldingheader = ({ regel, arbeidsgiver, sykmelder }: SykmeldingheaderProps) => {
-    return (
-        <div style={{ padding: '1rem' }}>
-            <Element>Årsak til manuell vurdering:</Element>
-            <Normaltekst style={{ marginBottom: '1.5rem' }}>{regel}</Normaltekst>
-            <Element>Arbeidsgiver: {arbeidsgiver}</Element>
-            <Element>Sykmelder: {sykmelder}</Element>
-        </div>
-    );
+  return (
+    <div style={{ padding: '1rem' }}>
+      <Element>Årsak til manuell vurdering:</Element>
+      <Normaltekst style={{ marginBottom: '1.5rem' }}>{RuleNamesDescription[regel]}</Normaltekst>
+      <Element>Arbeidsgiver: {arbeidsgiver}</Element>
+      <Element>Sykmelder: {sykmelder}</Element>
+    </div>
+  );
 };
 
 export default Sykmeldingheader;
