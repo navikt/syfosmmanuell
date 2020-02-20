f58bad04cfca3bcac137288ee577083a72f0d553
Jonas Henie <jonas.henie@nav.no>
2020-02-20 14:39:21 +0100

    Retter skrivefeil

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -83,7 +83,7 @@ const App = () => {
               if (GOSYS_URL) {
                 setTimeout(() => (window.location.href = GOSYS_URL), 1000);
               } else {
-                setFeilmelding('Oppagven ble ferdigstillt, men det var ikke mulig å sende deg tilbake til GOSYS');
+                setFeilmelding('Oppgaven ble ferdigstillt, men det var ikke mulig å sende deg tilbake til GOSYS');
               }
             }
           },
