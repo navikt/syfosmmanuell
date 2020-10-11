63d4abd93961c401427d3ce0480bcdd485adbb18
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-10-11 15:51:02 +0200

    Add feiloppsummering

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,11 +1,16 @@
 import { Knapp } from 'nav-frontend-knapper';
-import { RadioPanelGruppe } from 'nav-frontend-skjema';
-import React from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import { Feiloppsummering, FeiloppsummeringFeil, RadioPanelGruppe } from 'nav-frontend-skjema';
+import React, { useEffect, useRef } from 'react';
+import { useForm, Controller, DeepMap, FieldError } from 'react-hook-form';
 import { arsaker, FormShape } from '../../types/formTypes';
 import { Result } from '../../types/resultTypes';
 import './Form.less';
 import HvaViSierTilBehandlerOgPasient from './HvaViSierTilBehandlerOgPasient';
+
+const getFeilOppsummeringsfeil = (errors: DeepMap<FormShape, FieldError>): FeiloppsummeringFeil[] =>
+  Object.entries(errors).map(([key, value]) => ({ skjemaelementId: `b-${key}`, feilmelding: value?.message! }));
+
+const hasErrors = (errors: DeepMap<FormShape, FieldError>): boolean => !!Object.keys(errors).length;
 
 interface FormProps {
   ferdigstillOppgave: (result: Result) => void;
@@ -15,6 +20,7 @@ const Form = ({ ferdigstillOppgave }: FormProps) => {
   const { control, handleSubmit, errors, watch } = useForm<FormShape>();
   const watchGodkjent = watch('godkjent');
   const watchAvvisningstekst = watch('avvisningstekst');
+  const feiloppsummeringRef = useRef<HTMLDivElement>();
 
   const onSubmit = (data: FormShape) => {
     const result: Result = {
@@ -23,6 +29,12 @@ const Form = ({ ferdigstillOppgave }: FormProps) => {
     };
     ferdigstillOppgave(result);
   };
+
+  useEffect(() => {
+    if (hasErrors(errors)) {
+      feiloppsummeringRef?.current?.focus();
+    }
+  }, [errors]);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -39,7 +51,7 @@ const Form = ({ ferdigstillOppgave }: FormProps) => {
             checked={value}
             feil={errors.godkjent?.message}
             radios={[
-              { label: 'Godkjenn', value: 'true' },
+              { id: 'b-godkjent', label: 'Godkjenn', value: 'true' },
               { label: 'Avvis', value: 'false' },
             ]}
           />
@@ -59,7 +71,11 @@ const Form = ({ ferdigstillOppgave }: FormProps) => {
               checked={value}
               feil={errors.avvisningstekst?.message}
               radios={[
-                { label: arsaker.TILBAKEDATERT_FORLENGELSE.label, value: arsaker.TILBAKEDATERT_FORLENGELSE.key },
+                {
+                  id: 'b-avvisningstekst',
+                  label: arsaker.TILBAKEDATERT_FORLENGELSE.label,
+                  value: arsaker.TILBAKEDATERT_FORLENGELSE.key,
+                },
                 { label: arsaker.TILBAKEDATERT_FORSTE.label, value: arsaker.TILBAKEDATERT_FORSTE.key },
               ]}
             />
@@ -67,6 +83,14 @@ const Form = ({ ferdigstillOppgave }: FormProps) => {
         />
       )}
       <HvaViSierTilBehandlerOgPasient arsak={watchAvvisningstekst} />
+      {hasErrors(errors) && (
+        <Feiloppsummering
+          className="feiloppsummering"
+          innerRef={feiloppsummeringRef as any}
+          tittel="For å gå videre må du rette opp følgende"
+          feil={getFeilOppsummeringsfeil(errors)}
+        />
+      )}
       <Knapp type="hoved" htmlType="submit">
         Ferdigstill
       </Knapp>
