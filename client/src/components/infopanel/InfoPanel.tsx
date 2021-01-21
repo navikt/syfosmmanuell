20b65b263262255d8aaf96641e80ada763b39fea
Jonas Henie <jonas.henie@nav.no>
2021-01-21 19:39:25 +0100

    Restyle infopanel

--- client/src/components/infopanel/InfoPanel.tsx
+++ client/src/components/infopanel/InfoPanel.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Element } from 'nav-frontend-typografi';
+import { Undertittel } from 'nav-frontend-typografi';
 
 import plaster from '../../svg/plaster.svg';
 
@@ -8,23 +8,23 @@ import './infopanel.less';
 type Fargetema = 'feil' | 'info' | 'advarsel';
 
 interface InfoPanelProps {
-    fargetema: Fargetema;
-    tittel: string;
-    children: any | any[];
+  fargetema: Fargetema;
+  tittel: string;
+  children: any | any[];
 }
 
 const InfoPanel = ({ fargetema, tittel, children }: InfoPanelProps) => {
-    return (
-        <article className="infopanel">
-            <header className={`infopanel-header infopanel-${fargetema}`}>
-                <img className="infopanel-header-icon" src={plaster} alt="plasterikon" />{' '}
-                <span className="infopanel-header-tekst">
-                    <Element>{tittel}</Element>
-                </span>
-            </header>
-            <div className="infopanel-content">{children}</div>
-        </article>
-    );
+  return (
+    <article className="infopanel">
+      <header className={`infopanel-header infopanel-${fargetema}`}>
+        <img className="infopanel-header-icon" src={plaster} alt="plasterikon" />{' '}
+        <span className="infopanel-header-tekst">
+          <Undertittel>{tittel}</Undertittel>
+        </span>
+      </header>
+      <div className="infopanel-content">{children}</div>
+    </article>
+  );
 };
 
 export default InfoPanel;
