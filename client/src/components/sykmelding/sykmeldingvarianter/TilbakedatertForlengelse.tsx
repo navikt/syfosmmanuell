950e3519d854991eab9d4c905eb4a8128ffdde79
Jonas Henie <jonas.henie@nav.no>
2021-01-21 19:35:12 +0100

    Reduce utdrag

--- client/src/components/sykmelding/sykmeldingvarianter/TilbakedatertForlengelse.tsx
+++ client/src/components/sykmelding/sykmeldingvarianter/TilbakedatertForlengelse.tsx
@@ -3,13 +3,7 @@ import { Sykmelding } from '../../../types/sykmeldingTypes';
 import DiagnoseSeksjon from '../../infopanel/panelelementer/diagnose/DiagnoseSeksjon';
 import InfoPanel from '../../infopanel/InfoPanel';
 import SykmeldingPerioder from '../../infopanel/panelelementer/periode/SykmeldingPerioder';
-import BehandlingsDatoer from '../../infopanel/utdypendeelementer/BehandlingsDatoer';
-import Friskmelding from '../../infopanel/utdypendeelementer/Friskmelding';
-import Arbeidsevne from '../../infopanel/utdypendeelementer/Arbeidsevne';
-import UtdypendeOpplysninger from '../../infopanel/utdypendeelementer/UtdypendeOpplysninger';
 import Tilbakedateringsinfo from '../../infopanel/utdypendeelementer/Tilbakedateringsinfo';
-import SeksjonMedTittel from '../../infopanel/layout/SeksjonMedTittel';
-import ElementMedTekst from '../../infopanel/layout/ElementMedTekst';
 
 interface TilbakedatertForlengelseProps {
   sykmelding: Sykmelding;
@@ -19,22 +13,12 @@ interface TilbakedatertForlengelseProps {
 const TilbakedatertForlengelse = ({ sykmelding, personNrPasient }: TilbakedatertForlengelseProps) => {
   return (
     <InfoPanel tittel="Utdrag fra sykmeldingen" fargetema="advarsel">
-      <SeksjonMedTittel understrek>
-        <ElementMedTekst vis tittel="Fødselsnummer pasient" tekst={personNrPasient} margin />
-        <BehandlingsDatoer signaturDato={sykmelding.signaturDato} />
-        <Tilbakedateringsinfo
-          dokumenterbarKontaktDato={sykmelding.kontaktMedPasient.kontaktDato}
-          kanIkkeIvaretaEgneInteresser={sykmelding.kontaktMedPasient.begrunnelseIkkeKontakt}
-        />
-        <SykmeldingPerioder perioder={sykmelding.perioder} />
-        <DiagnoseSeksjon diagnose={sykmelding.medisinskVurdering.hovedDiagnose} />
-      </SeksjonMedTittel>
-      <Friskmelding prognose={sykmelding.prognose} />
-      <UtdypendeOpplysninger opplysninger={sykmelding.utdypendeOpplysninger} />
-      <Arbeidsevne
-        tiltakArbeidsplassen={sykmelding.tiltakArbeidsplassen}
-        tiltakNAV={sykmelding.tiltakNAV}
-        andreTiltak={sykmelding.andreTiltak}
+      <SykmeldingPerioder perioder={sykmelding.perioder} />
+      <DiagnoseSeksjon diagnose={sykmelding.medisinskVurdering.hovedDiagnose} />
+
+      <Tilbakedateringsinfo
+        dokumenterbarKontaktDato={sykmelding.kontaktMedPasient.kontaktDato}
+        kanIkkeIvaretaEgneInteresser={sykmelding.kontaktMedPasient.begrunnelseIkkeKontakt}
       />
     </InfoPanel>
   );
