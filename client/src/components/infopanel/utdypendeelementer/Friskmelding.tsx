8d868634d7bae4d7dec087a563bc72e13f014b48
Jonas Henie <jonas.henie@nav.no>
2021-01-28 17:49:40 +0100

    Update Friskmelding

--- client/src/components/infopanel/utdypendeelementer/Friskmelding.tsx
+++ client/src/components/infopanel/utdypendeelementer/Friskmelding.tsx
@@ -11,110 +11,67 @@ import ElementMedTekst from '../layout/ElementMedTekst';
 import EnkelCheckbox from '../layout/EnkelCheckbox';
 
 interface FriskmeldingProps {
-    prognose?: Prognose;
+  prognose?: Prognose;
 }
 
 const Friskmelding = ({ prognose }: FriskmeldingProps) => {
-    // TODO: legg til logikk for visning av seksjon
-    /*
-    const visSeksjon = (sykmelding.friskmelding.antarReturSammeArbeidsgiver ||
-    sykmelding.friskmelding.antattDatoReturSammeArbeidsgiver ||
-    sykmelding.friskmelding.antarReturAnnenArbeidsgiver ||
-    sykmelding.friskmelding.tilbakemeldingReturArbeid ||
-    sykmelding.friskmelding.utenArbeidsgiverTilbakemelding ||
-    sykmelding.friskmelding.utenArbeidsgiverAntarTilbakeIArbeid ||
-    sykmelding.friskmelding.utenArbeidsgiverAntarTilbakeIArbeidDato ||
-    sykmelding.friskmelding.utenArbeidsgiverTilbakemelding);
-    */
-    if (!prognose) {
-        return null;
-    }
+  if (!prognose) {
+    return null;
+  }
 
-    const { erIArbeid, erIkkeIArbeid, hensynArbeidsplassen, arbeidsforEtterPeriode } = prognose;
+  const { erIArbeid, erIkkeIArbeid } = prognose;
 
-    const ErIArbeidSeksjon = () => {
-        if (!erIArbeid) {
-            return null;
-        }
-
-        return (
-            <Margin>
-                <Margin>
-                    <EnkelCheckbox
-                        tittel={tekster['friskmelding.samme-arbeidsgiver']}
-                        bold
-                        margin
-                        checked={erIArbeid.egetArbeidPaSikt}
-                        vis={erIArbeid.egetArbeidPaSikt}
-                    />
-                    <ElementMedTekst
-                        vis={erIArbeid.egetArbeidPaSikt && !!erIArbeid.arbeidFOM}
-                        tittel={tekster['friskmelding.arbeidfom']}
-                        tekst={tilLesbarDatoMedArstall(erIArbeid.arbeidFOM)}
-                        innrykk
-                    />
-                </Margin>
-                <Margin>
-                    <EnkelCheckbox
-                        tittel={tekster['friskmelding.annen-arbeidsgiver']}
-                        bold
-                        margin
-                        checked={erIArbeid.annetArbeidPaSikt}
-                        vis={erIArbeid.annetArbeidPaSikt}
-                    />
-                    <ElementMedTekst
-                        vis={erIArbeid.annetArbeidPaSikt && !!erIArbeid.vurderingsdato}
-                        tittel={tekster['friskmelding.vurderingsdato']}
-                        tekst={tilLesbarDatoMedArstall(erIArbeid.vurderingsdato)}
-                        innrykk
-                    />
-                </Margin>
-            </Margin>
-        );
-    };
-
-    const ErIkkeIArbeidSeksjon = () => {
-        if (!erIkkeIArbeid) {
-            return null;
-        }
-
-        return (
-            <Margin>
-                <EnkelCheckbox
-                    tittel={tekster['friskmelding.ingen-arbeidsgiver']}
-                    bold
-                    margin
-                    checked={erIkkeIArbeid.arbeidsforPaSikt}
-                    vis={erIkkeIArbeid.arbeidsforPaSikt}
-                />
-                <ElementMedTekst
-                    vis={!!erIkkeIArbeid.arbeidsforFOM}
-                    tittel={tekster['friskmelding.arbeidfom']}
-                    tekst={tilLesbarDatoMedArstall(erIkkeIArbeid.arbeidsforFOM)}
-                />
-                <ElementMedTekst
-                    vis={!!erIkkeIArbeid.vurderingsdato}
-                    tittel={tekster['friskmelding.ingen-arbeidsgiver.vurdering']}
-                    tekst={tilLesbarDatoMedArstall(erIkkeIArbeid.vurderingsdato)}
-                />
-            </Margin>
-        );
-    };
-
-    return (
-        <SeksjonMedTittel understrek tittel={tekster['friskmelding.tittel']}>
-            <EnkelCheckbox
-                tittel={tekster['friskmelding.arbeidsfor.tittel']}
-                checked={arbeidsforEtterPeriode}
-                margin
-                bold
-                vis={arbeidsforEtterPeriode}
-            />
-            <ElementMedTekst tittel={tekster['friskmelding.hensyn']} tekst={hensynArbeidsplassen} margin />
-            <ErIArbeidSeksjon />
-            <ErIkkeIArbeidSeksjon />
-        </SeksjonMedTittel>
-    );
+  return (
+    <SeksjonMedTittel understrek tittel="Friskmelding/prognose">
+      {erIArbeid && (
+        <>
+          <ElementMedTekst
+            tittel="Kan pasienten på sikt komme tilbake til samme arbeidsgiver?"
+            tekst={erIArbeid.egetArbeidPaSikt ? 'Ja' : 'Nei'}
+            margin
+          />
+          <ElementMedTekst
+            vis={erIArbeid.egetArbeidPaSikt && !!erIArbeid.arbeidFOM}
+            tittel="Anslå når du tror dette kan skje"
+            tekst={tilLesbarDatoMedArstall(erIArbeid.arbeidFOM)}
+            margin
+          />
+          <ElementMedTekst
+            tittel="Kan pasienten på sikt komme i arbeid hos en annen arbeidsgiver?"
+            tekst={erIArbeid.annetArbeidPaSikt ? 'Ja' : 'Nei'}
+            margin
+          />
+          <ElementMedTekst
+            vis={erIArbeid.annetArbeidPaSikt && !!erIArbeid.vurderingsdato}
+            tittel="Når antar du å kunne gi tilbakemelding på dette?"
+            tekst={tilLesbarDatoMedArstall(erIArbeid.vurderingsdato)}
+            margin
+          />
+        </>
+      )}
+      {erIkkeIArbeid && (
+        <Margin>
+          <EnkelCheckbox
+            tittel={tekster['friskmelding.ingen-arbeidsgiver']}
+            bold
+            margin
+            checked={erIkkeIArbeid.arbeidsforPaSikt}
+            vis={erIkkeIArbeid.arbeidsforPaSikt}
+          />
+          <ElementMedTekst
+            vis={!!erIkkeIArbeid.arbeidsforFOM}
+            tittel={tekster['friskmelding.arbeidfom']}
+            tekst={tilLesbarDatoMedArstall(erIkkeIArbeid.arbeidsforFOM)}
+          />
+          <ElementMedTekst
+            vis={!!erIkkeIArbeid.vurderingsdato}
+            tittel={tekster['friskmelding.ingen-arbeidsgiver.vurdering']}
+            tekst={tilLesbarDatoMedArstall(erIkkeIArbeid.vurderingsdato)}
+          />
+        </Margin>
+      )}
+    </SeksjonMedTittel>
+  );
 };
 
 export default Friskmelding;
