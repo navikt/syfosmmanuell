92bd2613dcad6df923d42b8f2773b5d73783f0c7
Jonas Henie <jonas.henie@nav.no>
2021-01-29 13:23:55 +0100

    Undefined -> null

--- client/src/mock/data/manuellOppgave.ts
+++ client/src/mock/data/manuellOppgave.ts
@@ -110,12 +110,7 @@ export const manuellOppgave = {
         arbeidFOM: '2018-11-21',
         vurderingsdato: '2018-10-30',
       },
-      erIkkeIArbeid: undefined,
-      /* erIkkeIArbeid: {
-        arbeidsforPaSikt: true,
-        arbeidsforFom: '2019-11-11',
-        vurderingsdato: '2019-11-12',
-      } */
+      erIkkeIArbeid: null,
     },
     utdypendeOpplysninger: {
       '6.3': {
