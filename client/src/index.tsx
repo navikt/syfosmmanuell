b9c2a53e57749f84abaee12bb8791db1ca2ac425
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-02-12 17:23:00 +0100

    removes service worker code that comes default with create react app because we dont need this app to work offline

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import React from 'react';
 import './index.less';
-import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import NAVSPA from '@navikt/navspa';
 import StoreProvider from './data/store';
@@ -24,7 +23,3 @@ const Wrapper = () => {
 };
 
 NAVSPA.eksporter('wrapper', Wrapper);
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
