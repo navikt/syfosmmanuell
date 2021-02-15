ad95e9e84742ba483af4ba9ed8fdc39cc88accc6
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-02-15 13:08:19 +0100

    Make demoapp receive mock data

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,7 @@ import NAVSPA from '@navikt/navspa';
 import StoreProvider from './data/store';
 import DecoratorWrapper from './components/Decorator';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' || process.env.REACT_APP_DEMO === 'true') {
   require('./mock');
 }
 
