6638359a576616167225f7cfa2c7cefbf1fdc6c7
Sebastian <sebastian.knudsen@nav.no>
2021-02-15 13:40:11 +0100

    Revert (#89)
    * Revert "Make demoapp receive mock data"
    
    This reverts commit ad95e9e84742ba483af4ba9ed8fdc39cc88accc6.
    
    * Revert "Refactor (#88)"
    
    This reverts commit 079cc2aacba88ea0efdc8563f5223390a6017070.

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,53 +1,137 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ManuellOppgave } from '../types/manuellOppgaveTypes';
+import { hentOppgaveidFraUrlParameter } from '../utils/urlUtils';
 import Spinner from 'nav-frontend-spinner';
 import { Normaltekst } from 'nav-frontend-typografi';
 import MainContent from './MainContent';
-import { ApiError, hentOppgave } from '../utils/dataUtils';
-import { StoreContext } from '../data/store';
-import { Knapp } from 'nav-frontend-knapper';
+import { FormShape } from './form/Form';
 
-const App = () => {
-  const { state, dispatch } = useContext(StoreContext);
-  const { manuellOppgave, isLoading, isCompleted, error } = state;
+interface AppProps {
+  enhet: string | null | undefined;
+}
+
+const App = ({ enhet }: AppProps) => {
+  const [manOppgave, setManOppgave] = useState<ManuellOppgave | null | undefined>(undefined);
+  const [apiError, setApiError] = useState<Error | null>(null);
+  const [missingEnhetError, setMissingEnhetError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isCompleted, setIsCompleted] = useState<boolean>(false);
+
+  // Need to store enhet in a ref becuase setTimeout reads value of "enhet" at declaration
+  const enhetRef = useRef(enhet);
+  enhetRef.current = enhet;
+
+  // if enhet is not passed from the decorator within 10s -> display error
+  useEffect(() => {
+    if (enhet) {
+      setMissingEnhetError(null);
+    } else {
+      const timer = setTimeout(() => {
+        if (!enhetRef.current) {
+          setMissingEnhetError(
+            new Error('Feil ved henting av valgt enhet. Har du husket å velge enhet i menyen øverst på siden?'),
+          );
+        }
+      }, 10000);
+      return () => clearTimeout(timer);
+    }
+  }, [enhet]);
+
+  const hentOppgave = () => {
+    try {
+      const OPPGAVE_ID = hentOppgaveidFraUrlParameter(window.location.href);
+      const URL = `/backend/api/v1/manuellOppgave/${OPPGAVE_ID}`;
+      setIsLoading(true);
+      fetch(URL, {
+        credentials: 'same-origin',
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else if (response.status === 401) {
+            throw new Error(
+              'Kunne ikke hente oppgave på grunn av autorisasjonsfeil. Sjekk med din leder om du har tilgang til å vurdere manuelle oppgaver',
+            );
+          } else if (response.status === 204) {
+            throw new Error('Oppgaven du prøver å hente er allerede løst');
+          } else {
+            throw new Error(`Feil ved henting av oppgave. Feilkode: ${response.status}`);
+          }
+        })
+        .then((oppgave) => {
+          setManOppgave(new ManuellOppgave(oppgave));
+        })
+        .catch((error) => {
+          console.error(error);
+          setApiError(error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    } catch (error) {
+      console.error(error);
+      setApiError(error);
+    }
+  };
+
+  const ferdigstillOppgave = (result: FormShape) => {
+    setIsLoading(true);
+    const URL = `/backend/api/v1/vurderingmanuelloppgave/${manOppgave!.oppgaveid}`;
+    fetch(URL, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: { 'Content-Type': 'application/json', 'X-Nav-Enhet': enhet! },
+      body: JSON.stringify(result),
+    })
+      .then((response) => {
+        if (response.ok) {
+          setIsCompleted(true);
+          const GOSYS_URL = process.env.REACT_APP_GOSYS_URL;
+          if (GOSYS_URL) {
+            setTimeout(() => (window.location.href = GOSYS_URL), 1000);
+          } else {
+            throw new Error('Oppgaven ble ferdigstilt, men det var ikke mulig å sende deg tilbake til GOSYS');
+          }
+        } else if (response.status === 401) {
+          throw new Error(
+            'Kunne ikke vurdere oppgaven på grunn av autorisasjonsfeil. Sjekk med din leder om du har tilgang til å vurdere manuelle oppgaver',
+          );
+        } else {
+          throw new Error(`Feil ved ferdigstilling av oppgaven. Feilkode: ${response.status}`);
+        }
+      })
+      .catch((error) => {
+        setApiError(error);
+        console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   useEffect(() => {
-    if (!manuellOppgave) {
-      (async () => {
-        dispatch({ type: 'FETCHING' });
-        try {
-          const OPPGAVE_ID = new URL(window.location.href).searchParams.get('oppgaveid');
-          if (OPPGAVE_ID === null) {
-            throw new Error('Oppgaveid mangler i lenken.');
-          } else {
-            const manuellOppgaveRawJson = await hentOppgave(OPPGAVE_ID);
-            const manuellOppgave = new ManuellOppgave(manuellOppgaveRawJson);
-
-            dispatch({ type: 'TASK_LOADED', payload: manuellOppgave });
-          }
-        } catch (error) {
-          console.error(error);
-          if (error instanceof ApiError) {
-            dispatch({ type: 'ERROR', payload: error });
-          } else if (error instanceof TypeError) {
-            dispatch({ type: 'ERROR', payload: new Error('Det oppsto en feil med oppgaven.') });
-          } else {
-            dispatch({ type: 'ERROR', payload: new Error('En ukjent feil oppsto.') });
-          }
-        }
-      })();
+    if (!manOppgave) {
+      hentOppgave();
     }
-  }, [manuellOppgave]);
+  }, [manOppgave]);
 
-  if (error) {
+  if (apiError) {
     return (
       <div className="margin-top--2">
-        <Normaltekst>{error.message}</Normaltekst>
+        <Normaltekst>{apiError.message}</Normaltekst>
       </div>
     );
   }
 
-  if (isLoading) {
+  if (missingEnhetError) {
+    return (
+      <div className="margin-top--2">
+        <Normaltekst>{missingEnhetError.message}</Normaltekst>
+      </div>
+    );
+  }
+
+  if (isLoading || !enhet) {
     return (
       <div className="margin-top--2">
         <Spinner />
@@ -58,27 +142,13 @@ const App = () => {
   if (isCompleted) {
     return (
       <div className="margin-top--2">
-        <Normaltekst>Oppgaven er løst.</Normaltekst>
-        <Knapp
-          type="hoved"
-          className="margin-top--2"
-          onClick={() => {
-            const GOSYS_URL = process.env.REACT_APP_GOSYS_URL;
-            if (GOSYS_URL) {
-              window.location.href = GOSYS_URL;
-            } else {
-              throw new Error('Det oppsto en feil ved da vi forsøkte å sende deg til GOSYS.');
-            }
-          }}
-        >
-          Tilbake til GOSYS
-        </Knapp>
+        <Normaltekst>Oppgaven er løst... Du videresendes til GOSYS</Normaltekst>
       </div>
     );
   }
 
-  if (manuellOppgave) {
-    return <MainContent manuellOppgave={manuellOppgave} />;
+  if (manOppgave) {
+    return <MainContent manuellOppgave={manOppgave} ferdigstillOppgave={ferdigstillOppgave} />;
   }
 
   return <p>Ukjent feil</p>;
---
+++ client/src/utils/urlUtils.ts
@@ -0,0 +1,51 @@
+export class UrlError extends Error {}
+
+export const hentOppgaveidFraUrlParameter = (url: string): string => {
+  if (
+    process.env.NODE_ENV === 'development' ||
+    process.env.NODE_ENV === 'test' ||
+    process.env.REACT_APP_NODE_ENV === 'development'
+  ) {
+    console.info('Du befinner deg i development og vil derfor motta mock-data');
+    return '';
+  }
+  const splitter = url.split('?');
+  if (splitter.length === 1) {
+    throw new UrlError('Url does not contain any parameters');
+  }
+  if (splitter.length > 2) {
+    throw new UrlError('Url contains several "?"');
+  }
+  const params = splitter[1].split('&');
+  if (params.length > 1) {
+    throw new UrlError('Url contains too many parameters');
+  }
+  const param = params[0].split('=');
+  if (param.length === 1 || param[1] === '') {
+    throw new UrlError('Parameter does not contain any value');
+  }
+  if (param[0] === 'oppgaveid') {
+    return param[1];
+  } else {
+    throw new UrlError('The parameter given is not oppgaveid');
+  }
+};
+
+export const hentLogInUrl = (): string => {
+  if (process.env.NODE_ENV === 'development') {
+    return 'localhost:3000';
+  }
+  try {
+    const OPPGAVE_ID = localStorage.getItem('OPPGAVE_ID');
+    if (!OPPGAVE_ID) {
+      throw new Error('Kunne ikke finne OPPGAVE_ID i localStorage');
+    }
+    const url = process.env.REACT_APP_WEB_SERVER_URL;
+    if (!url) {
+      throw new Error('Kunne ikke finne url til å logge inn');
+    }
+    return `${url}?oppgaveid=${OPPGAVE_ID}`;
+  } catch (error) {
+    throw error;
+  }
+};
