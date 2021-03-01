27d2bc0834861f58fca5865f983558d18246bc33
Jonas Henie <jonas.henie@nav.no>
2021-03-01 13:54:14 +0100

    Re-add "Andre innspill" (#96)

--- client/src/components/sykmelding/sykmeldingvarianter/HeleSykmeldingen.tsx
+++ client/src/components/sykmelding/sykmeldingvarianter/HeleSykmeldingen.tsx
@@ -60,7 +60,11 @@ const HeleSykmeldingen = ({ sykmelding, setVisHeleSykmeldingen }: HeleSykmelding
       <MulighetForArbeid perioder={sykmelding.perioder} />
       <Friskmelding prognose={sykmelding.prognose} />
       <UtdypendeOpplysninger opplysninger={sykmelding.utdypendeOpplysninger} />
-      <Arbeidsevne tiltakArbeidsplassen={sykmelding.tiltakArbeidsplassen} tiltakNAV={sykmelding.tiltakNAV} />
+      <Arbeidsevne
+        tiltakArbeidsplassen={sykmelding.tiltakArbeidsplassen}
+        tiltakNAV={sykmelding.tiltakNAV}
+        andreTiltak={sykmelding.andreTiltak}
+      />
       <Annet
         meldingTilNAV={sykmelding.meldingTilNAV}
         meldingTilArbeidsgiver={sykmelding.meldingTilArbeidsgiver}
