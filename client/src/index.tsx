3e32fa5514d0ecd985240ec70f9dae273bff6ac5
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-06-30 16:00:08 +0200

    Remove shady mounting of application

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,9 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.less';
 import App from './components/App';
-import NAVSPA from '@navikt/navspa';
 import StoreProvider from './data/store';
 import DecoratorWrapper from './components/Decorator';
 import { setupLogger } from './utils/logger';
@@ -14,15 +14,12 @@ if (process.env.NODE_ENV === 'development' || process.env.REACT_APP_DEMO === 'tr
 
 setupLogger();
 
-const Wrapper = () => {
-  return (
-    <StoreProvider>
-      <DecoratorWrapper />
-      <main>
-        <App />
-      </main>
-    </StoreProvider>
-  );
-};
-
-NAVSPA.eksporter('wrapper', Wrapper);
+ReactDOM.render(
+  <StoreProvider>
+    <DecoratorWrapper />
+    <main>
+      <App />
+    </main>
+  </StoreProvider>,
+  document.getElementById('root'),
+);
