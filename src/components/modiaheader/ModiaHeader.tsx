f688d9a0199d4432810b6b5c2bdc9381dd01ad93
Natalie Uranes <natalie.uranes@nav.no>
2025-07-04 12:30:37 +0200

    fix: reflow

--- src/components/modiaheader/ModiaHeader.tsx
+++ src/components/modiaheader/ModiaHeader.tsx
@@ -20,43 +20,45 @@ function ModiaHeader({ modiaContext }: Props): ReactElement {
             <InternalHeader.Title as={Link} href="/oppgaver">
                 syfosmmanuell
             </InternalHeader.Title>
-            {modiaContext && !('errorType' in modiaContext) && (
-                <div className="-mt-2 ml-auto mr-2 flex items-center">
-                    {aktivEnhet && modiaContext.enheter.length ? (
-                        <Select
-                            id="modia-header"
-                            value={aktivEnhet}
-                            label="Velg enhet"
-                            hideLabel
-                            className="mt-2"
-                            size="small"
-                            onChange={(event) => {
-                                setAktivEnhet(event.target.value)
-                            }}
-                        >
-                            {modiaContext.enheter.map((it) => (
-                                <option key={it.enhetId} value={it.enhetId}>
-                                    {it.enhetId} {it.navn}
-                                </option>
-                            ))}
-                        </Select>
-                    ) : (
-                        <div className="mr-4 mt-2 flex items-center justify-center">
-                            <Label>Fant ingen enheter</Label>
-                        </div>
-                    )}
-                </div>
-            )}
-            {modiaContext && 'errorType' in modiaContext && (
-                <div className="mr-4 flex items-center justify-center">
-                    <Heading size="small" level="3">
-                        ⚠ Feil ved lasting av enheter
-                    </Heading>
-                </div>
-            )}
-            {modiaContext && !('errorType' in modiaContext) && (
-                <InternalHeader.User name={modiaContext.fornavn + ' ' + modiaContext.etternavn} />
-            )}
+            <div className="flex flex-wrap">
+                {modiaContext && !('errorType' in modiaContext) && (
+                    <div className="-mt-2 ml-auto mr-2 flex items-center">
+                        {aktivEnhet && modiaContext.enheter.length ? (
+                            <Select
+                                id="modia-header"
+                                value={aktivEnhet}
+                                label="Velg enhet"
+                                hideLabel
+                                className="mt-2"
+                                size="small"
+                                onChange={(event) => {
+                                    setAktivEnhet(event.target.value)
+                                }}
+                            >
+                                {modiaContext.enheter.map((it) => (
+                                    <option key={it.enhetId} value={it.enhetId}>
+                                        {it.enhetId} {it.navn}
+                                    </option>
+                                ))}
+                            </Select>
+                        ) : (
+                            <div className="mr-4 mt-2 flex items-center justify-center">
+                                <Label>Fant ingen enheter</Label>
+                            </div>
+                        )}
+                    </div>
+                )}
+                {modiaContext && 'errorType' in modiaContext && (
+                    <div className="mr-4 flex items-center justify-center">
+                        <Heading size="small" level="3">
+                            ⚠ Feil ved lasting av enheter
+                        </Heading>
+                    </div>
+                )}
+                {modiaContext && !('errorType' in modiaContext) && (
+                    <InternalHeader.User name={modiaContext.fornavn + ' ' + modiaContext.etternavn} />
+                )}
+            </div>
         </InternalHeader>
     )
 }
